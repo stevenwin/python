1eea09ab0f19ce18cd13b5e619ee8fd46453ab75
stenguye <stenguye@gmail.com>
2016-08-19 18:15:46 -0400

    Scraped first name of fighters

--- random/bs.py
+++ random/bs.py
@@ -6,12 +6,13 @@ import requests
 with open("fm.html") as soup_file:
 	soup_fm = BeautifulSoup(soup_file, "html5lib")
 
+fm_tbody = soup_fm.find("tbody")
 fm_table = soup_fm.find("table", {"class": "b-statistics__table"})
-fm_tr = fm_table.find_all("tr", {"class": "b-statistics__table-row"})
+fm_tr = fm_tbody.find_all("tr", {"class": "b-statistics__table-row"})
 fm_td = fm_table.find_all("td", {"class": "b-statistics__table-col"})
 
-for td in fm_td:
-	print td.string
+for tr in fm_tr:
+	print "First Name: " + tr.td.get_text().strip()
 
 
 
